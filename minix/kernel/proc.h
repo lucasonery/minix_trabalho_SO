57ff5e83b1ef2e654f60620269bab8c8561aa4cc
Vinicius Correa <128077654+Viniicorrea@users.noreply.github.com>
2025-06-28 17:57:34 -0300

    Update proc.h

--- minix/kernel/proc.h
+++ minix/kernel/proc.h
@@ -20,6 +20,7 @@
 #include "priv.h"
 
 struct proc {
+  u64_t p_remaining_time;
   struct stackframe_s p_reg;	/* process' registers saved in stack frame */
   struct segframe p_seg;	/* segment descriptors */
   proc_nr_t p_nr;		/* number of this process (for fast access) */
